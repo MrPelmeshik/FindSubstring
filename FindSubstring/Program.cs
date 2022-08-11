99a002ce8e83c0ff3a6f8dcb06277fcdd9f6e2e2
Ilya Fedorov <74106915+MrPelmeshik@users.noreply.github.com>
2022-08-11 21:08:23 +0500

    init

---
+++ FindSubstring/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace FindSubstring
+{
+    public static class Program
+    {
+        public static void Main(string[] argv)
+        {
+            
+        }
+    }
+}
